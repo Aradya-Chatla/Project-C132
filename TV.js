f6f04f49b7238d1ff88656d3f93ed5771087e36b
Aradya-Chatla <93376716+Aradya-Chatla@users.noreply.github.com>
2022-06-22 17:48:23 +0530

    Add files via upload

---
+++ TV.js
@@ -0,0 +1,30 @@
+Status = "";
+mango_image = "";
+
+function preload(){
+    mango_image = loadImage("TV.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(mango_image,gotResults);
+}
+
+function gotResults(error,relts){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(mango_image,0,0,640,350);
+}
